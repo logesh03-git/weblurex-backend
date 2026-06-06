85ff9be42d1cc812446099ae52087f9e770df73a
logesh03-git <logeahlogesh06@gmail.com>
2026-06-06 18:40:58 +0530

    checking the mail issue 3

--- index.js
+++ index.js
@@ -34,17 +34,15 @@ app.post("/send", async (req, res) => {
   });
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
